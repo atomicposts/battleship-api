d1c36a024e835a562d8b0ee4cfa65f9ef7524201
Javier Núñez <javier.nunez@solucionex.com>
2016-11-20 00:21:29 +0100

    Request get board multi dimension

--- server.js
+++ server.js
@@ -6,14 +6,25 @@ app.use(bodyParser.json())
 
 var board = null
 
+var matrix = function ( rows, cols, defaultValue){
+    var arr = [];
+    // Creates all lines:
+    for(var i=0; i < rows; i++){
+        // Creates an empty line
+        arr.push([]);
+        // Adds cols to the empty line:
+        arr[i].push( new Array(cols));
+        for(var j=0; j < cols; j++){
+            // Initializes:
+            arr[i][j] = defaultValue;
+        }
+    }
+    return arr;
+}
+
 // API REST
-
-app.get('/', function (req, res) {
-    res.send('prueba')
-})
-
-app.get('/customers', function (req, res) {
-    res.json(board)
+app.get('/board/:rows/:columns', function (req, res) {
+    res.json(matrix(req.params.rows, req.params.columns, {'state': 0}))
 })
 
 app.listen(3000, function () {
